468b72753c05e57cfc8abd9e226b93f60981549f
Teodor Totev <teoet6@protonmail.com>
2025-07-02 14:45:46 +0300

    Structural changes and bridge table

---
+++ compile.py
@@ -0,0 +1,37 @@
+import html
+from pathlib import Path
+import shutil
+from lat2cyr import lat2cyr
+
+with open(Path('template')) as f:
+	template = f.read()
+
+dist = Path('dist')
+shutil.rmtree(dist)
+dist.mkdir()
+
+shutil.copy2(Path('style.css'), dist)
+
+postlist=''
+
+for it in reversed(sorted(Path('posts').iterdir())):
+	date = it.name[:10]
+	id = it.name[11:]
+	title = lat2cyr(id).replace('-', ' ').title()
+
+	with open(it) as f:
+		content = f.read()
+
+	output = template.format(title=html.escape(title), content=content, date=html.escape(date))
+
+	(dist/id).mkdir()
+	with open(dist/id/'index.html', 'w') as f:
+		f.write(output)
+
+	postlist += '<h3><a href={id}>{title}</a></h3>'.format(id=html.escape(id), title=html.escape(title))
+
+with open(Path('index')) as f:
+	content = f.read()
+output = template.format(title='Съдържание', content=content.format(postlist=postlist))
+with open(dist/'index.html', 'w') as f:
+	f.write(output)
